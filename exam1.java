9ab2aefa4c8b1a5d3f38c4adc1b8815618b6cf7b
apcsstrugglers <96290838+apcsstrugglers@users.noreply.github.com>
2021-12-17 18:27:41 +0800

    Add files via upload

---
+++ exam1.java
@@ -0,0 +1,36 @@
+1. 
+Complete the constructors and the Area method of the Circle class. (7 points)
+
+ public class Circle
+ {
+
+      private double radius;
+
+      // constructors
+      // postcondition: the instance variable is initialized
+      public Circle(double rad)
+      {
+
+      }
+
+      // postcondition: the instance variable is initialized
+      public Circle(int diameter)
+      {
+
+      }
+
+      // postcondition: returns the area of a circle with radius equal to
+      // this circle's radius, according to the formula: PI * r^2,
+      // where deg is the angle in degrees passed as the parameter,
+      // PI is the value of pi (3.1415), r is the radius of the circle,
+      // and "^2" means raised to the second power.
+      // Use the Math class constant to represent the value of pi.
+      public double Area(double degree)
+      {
+
+      }
+
+      // There may be other instance variables, constructors,
+      // and methods that are not shown.
+
+ } 
